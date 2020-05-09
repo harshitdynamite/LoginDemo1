5cbf9b24cdd3f0e4ea904539283b24adb7237da2
Harshit Agarwal <harshitdynamite@gmail.com>
2020-05-09 20:12:11 +0530

    Boiler plate+ swagger + initial commit 2

---
+++ LoginDemo/Controllers/TestController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LoginDemo.Controllers
+{
+    public class TestController : Controller
+    {
+        [HttpGet("api/User")]
+        public IActionResult Get()
+        {
+            return Ok(new { name = "Harhsit" });
+        }
+    }
+}
